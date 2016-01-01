0d817817f0f359f4f5d98d1d148716d35a983638
Jonathan Damico <jj_damico@yahoo.com>
2015-12-31 18:21:42 -0800

    Update js.js

--- js.js
+++ js.js
@@ -1,14 +1,12 @@
-/*
+$(document).ready(function () {
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    $('#resolution').replaceWith(''+windowWidth+'X'+windowHeight);
+});
+
 $(window).resize( function(){
   var windowWidth = $(window).width();
   var windowHeight = $(window).height();
   $('#resolution').replaceWith(''+windowWidth+'X'+windowHeight);
 });
-*/
-$(document).ready(function () {
-  while(true){
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
-    $('#resolution').replaceWith(''+windowWidth+'X'+windowHeight);
-  }
-});
+
