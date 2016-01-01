c998f4bfe9e6c2055e0a15d0348c6dd7f8d137bb
Jonathan Damico <jj_damico@yahoo.com>
2015-12-31 18:08:17 -0800

    Create js.js

---
+++ js.js
@@ -0,0 +1,10 @@
+/*
+$(window).resize( function(){
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).width();
+  $('#resolution').replaceWith(''+windowWidth+'X'+windowHeight);
+});
+*/
+$(document).ready(function () {
+  $('#resoluion').append('test');
+});
