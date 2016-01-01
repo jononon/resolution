c9053d83238955edd7b165a5b1915ec707bae029
Jonathan Damico <jj_damico@yahoo.com>
2015-12-31 18:23:44 -0800

    Update js.js

--- js.js
+++ js.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
     var windowWidth = $(window).width();
     var windowHeight = $(window).height();
-    $('#resolution').replaceWith(''+windowWidth+'X'+windowHeight);
+    $('#resolution').text(''+windowWidth+'X'+windowHeight);
 });
 
 $(window).resize( function(){
   var windowWidth = $(window).width();
   var windowHeight = $(window).height();
-  $('#resolution').replaceWith(''+windowWidth+'X'+windowHeight);
+  $('#resolution').text(''+windowWidth+'X'+windowHeight);
 });
-
